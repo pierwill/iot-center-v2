f3c72f5b36d1ee65ba310a42ad2268d0c71d01af
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-08-26 12:58:41 +0200

    feat: add antd

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Layout, Menu, Typography} from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        ToDo
-      </header>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed as boolean} onCollapse={() => setCollapsed(!collapsed)} >
+          <Header className="site-layout-background" style={{ padding: '10px' }}><h1 style={{color:'rgba(255,255,255,255)'}}>IoT Center</h1></Header>
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1">
+              UC 1
+            </Menu.Item>
+            <Menu.Item key="2">
+              UC 2
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout" style={{padding: 24}}>
+          <Title>UC1 Content Header</Title>
+          <Content>
+            <div
+              className="site-layout-background"
+              style={{ minHeight: 360 }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Link to Source Code
+          </Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
