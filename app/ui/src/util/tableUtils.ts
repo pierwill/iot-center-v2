3d88de5893e5a076d60527802678cac41988c595
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-12-07 10:53:26 +0100

    fix(app/ui): repair typescript conversion issue

--- app/ui/src/util/tableUtils.ts
+++ app/ui/src/util/tableUtils.ts
@@ -7,5 +7,10 @@ export const getXDomainFromTable = (
   table: GirrafeTable | undefined
 ): [number, number] | undefined => {
   const sorted = table?.getColumn('_time')?.slice()?.sort()
-  if (sorted) return [sorted[0], sorted[sorted.length - 1]] as [number, number]
+  if (sorted) {
+    return ([sorted[0], sorted[sorted.length - 1]] as unknown) as [
+      number,
+      number
+    ]
+  }
 }
