af65604631b8d28c0da8bd821401847a3463429c
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2021-02-18 19:14:46 +0100

    Update script files to be more transparent

--- app/run.sh
+++ app/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #export INFLUX_URL=https://us-west-2-1.aws.cloud2.influxdata.com
-export INFLUX_URL=
 #export INFLUX_TOKEN=h14b3X2n4kc8Q_jYPpwdjkv3dAZRorNQnN67pMwKs1lGgbMW8vWRjAi7VvkUitQMii2XwJM9qX3cnK4oAZDIjg==
-export INFLUX_TOKEN=
 #export INFLUX_ORG=IoTCenter
+
+export INFLUX_URL=
+export INFLUX_TOKEN=
 export INFLUX_ORG=
 
 set -euo pipefail
