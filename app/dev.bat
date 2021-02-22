f473112bc46c5035e1978ce1432ce33c61191a09
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2021-02-22 11:09:19 +0100

    Update script files to be more transparent

--- app/dev.bat
+++ app/dev.bat
@@ -1,10 +1,14 @@
 REM https://github.com/bonitoo-io/iot-center-v2
 
 REM SET INFLUX_URL=https://us-west-2-1.aws.cloud2.influxdata.com
-SET INFLUX_URL=<copy url from your web browser>
+REM   copy url from your web browser
 REM SET INFLUX_TOKEN=h14b3X2n4kc8Q_jYPpwdjkv3dAZRorNQnN67pMwKs1lGgbMW8vWRjAi7VvkUitQMii2XwJM9qX3cnK4oAZDIjg==
-SET INFLUX_TOKEN=<generated token in the InfluxDB UI>
+REM   generated token in the InfluxDB UI
 REM SET INFLUX_ORG=iotCenter@influxdata.com
-SET INFLUX_ORG=<your email>
+REM   typically your email (can be changed)
+
+SET INFLUX_URL=
+SET INFLUX_TOKEN=
+SET INFLUX_ORG=
 
 yarn dev
